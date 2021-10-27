0b74815d68ff9a62a2bded70279694bc341467af
Pawel <fordcrown2009@gmail.com>
2021-10-28 00:41:02 +0200

    Fixed problems with uppercase utf8 characters

--- rp_chat/sourceS.lua
+++ rp_chat/sourceS.lua
@@ -8,19 +8,51 @@ addEventHandler("onPlayerChat", root, function(message, messageType)
     cancelEvent()
 end)
 
-function correctText(text, uppercase)
+local upperCasedCharacters = {
+    ["a"] = "A", ["ą"] = "Ą",
+    ["b"] = "B", ["c"] = "C",
+    ["d"] = "D", ["e"] = "E",
+    ["ę"] = "Ę", ["f"] = "F",
+    ["g"] = "G", ["h"] = "H",
+    ["i"] = "I", ["j"] = "J",
+    ["k"] = "K", ["l"] = "L",
+    ["ł"] = "Ł", ["m"] = "M",
+    ["n"] = "N", ["ń"] = "Ń",
+    ["o"] = "O", ["ó"] = "Ó",
+    ["p"] = "P", ["q"] = "Q",
+    ["r"] = "R", ["s"] = "S",
+    ["ś"] = "Ś", ["t"] = "T",
+    ["u"] = "U", ["v"] = "V",
+    ["w"] = "W", ["x"] = "X",
+    ["y"] = "Y", ["z"] = "Z",
+    ["ż"] = "Ż", ["ź"] = "Ź"
+}
+
+function correctText(text, extra)
     if text then
         local endLetter = string.sub(text, -1)
 
-        if endLetter ~= "." and endLetter ~= "?" and endLetter ~= "!" then
-            text = text.."."
+        if extra ~= 2 then
+            if endLetter ~= "." and endLetter ~= "?" and endLetter ~= "!" then
+                text = text.."."
+            end
         end
 
-        if uppercase == true then
+        if extra == 1 then
             return text
         end
 
-        return text:gsub("^%l", string.upper)
+        local upperCasedFirstLetter = upperCasedCharacters[utf8.sub(text, 1, 1)]
+        if upperCasedFirstLetter ~= nil then
+            return upperCasedFirstLetter..utf8.sub(text, 2)
+        end
+
+        upperCasedFirstLetter = upperCasedCharacters[string.sub(text, 1, 1)]
+        if upperCasedFirstLetter then
+            return upperCasedFirstLetter..utf8.sub(text, 2)
+        end
+
+        return text
     end
 end
 
@@ -48,23 +80,22 @@ function outputMessage(player, type, text)
             if type == 1 then -- mówienie
                 outputChatBox(exports.rp_systems:getCharacterData(currentCharacter, "name").. " mówi: " ..correctText(text), getPlayersInRange(player, 10), 255, 255, 255)
             elseif type == 2 then -- /me
-                outputChatBox("** " ..exports.rp_systems:getCharacterData(currentCharacter, "name").. " " ..correctText(text, true), getPlayersInRange(player, 15), 220, 162, 244)
+                outputChatBox("** " ..exports.rp_systems:getCharacterData(currentCharacter, "name").. " " ..correctText(text, 1), getPlayersInRange(player, 15), 220, 162, 244)
             elseif type == 3 then -- /do
                 outputChatBox("** " ..correctText(text).. " (( " ..exports.rp_systems:getCharacterData(currentCharacter, "name").. " )) **", getPlayersInRange(player, 15), 137, 130, 189)
             elseif type == 4 then -- szept
                 outputChatBox(exports.rp_systems:getCharacterData(currentCharacter, "name").. " szepcze: " ..correctText(text), getPlayersInRange(player, 5), 255, 255, 255)
             elseif type == 5 then -- krzyk
-                outputChatBox(exports.rp_systems:getCharacterData(currentCharacter, "name").. " krzyczy: " ..text:gsub("^%l", string.upper).. "!", getPlayersInRange(player, 20), 255, 255, 255)
+                outputChatBox(exports.rp_systems:getCharacterData(currentCharacter, "name").. " krzyczy: " ..correctText(text, 2).. "!", getPlayersInRange(player, 20), 255, 255, 255)
             elseif type == 6 then -- ooc
                 outputChatBox("(( " ..exports.rp_systems:getCharacterData(currentCharacter, "name").. ": " ..text.. " ))", getPlayersInRange(player, 15), 171, 171, 171)
             elseif type == 7 then -- /sprobuj
-                print(true)
-
                 local characterUID = exports.rp_systems:getPlayerCurrentCharacter(player)
                 local characterGender = exports.rp_systems:getCharacterGender(characterUID)
-                iprint(characterUID, characterGender)
 
-                outputChatBox("* " ..exports.rp_systems:getCharacterData(currentCharacter, "name").. " " ..genderStrings[characterGender][math.random(#genderStrings[characterGender])].. " próbując "..correctText(text, true), getPlayersInRange(player, 15), 220, 162, 244)
+                if not characterUID or not characterGender then return end
+
+                outputChatBox("* " ..exports.rp_systems:getCharacterData(currentCharacter, "name").. " " ..genderStrings[characterGender][math.random(#genderStrings[characterGender])].. " próbując "..correctText(text, 1), getPlayersInRange(player, 15), 220, 162, 244)
             end
         end
     end
